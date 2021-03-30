304b1c12288985bf41db758ec102b286cbc35483
sunfusheng <sunfusheng1989@126.com>
2021-03-30 11:09:00 +0800

    add TopAppBar

--- app/src/main/java/learn/jetpack/compose/app/MainActivity.kt
+++ app/src/main/java/learn/jetpack/compose/app/MainActivity.kt
@@ -3,12 +3,12 @@ package learn.jetpack.compose.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import learn.jetpack.compose.app.ui.theme.LearnJetpackComposeTheme
+import learn.jetpack.compose.app.ui.theme.ComposeMaterialTheme
+import learn.jetpack.compose.app.ui.theme.PrimaryColor
 
 class MainActivity : ComponentActivity() {
 
@@ -20,13 +20,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HomePage()
+}
+
 @Composable
 fun HomePage() {
-    LearnJetpackComposeTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Text(text = "Hello Jetpack Compose")
-        }
+    ComposeMaterialTheme {
+        TopAppBar(
+            backgroundColor = PrimaryColor,
+            title = { Text("Learn Jetpack Compose") }
+        )
     }
-
 }
--- app/src/main/java/learn/jetpack/compose/app/ui/theme/Theme.kt
+++ app/src/main/java/learn/jetpack/compose/app/ui/theme/Theme.kt
@@ -1,44 +1,22 @@
 package learn.jetpack.compose.app.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
-)
 
-private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val LightColor = lightColors(
+    primary = PrimaryColor,
+    primaryVariant = PrimaryColor,
+    secondary = PrimaryColor
 )
 
 @Composable
-fun LearnJetpackComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun ComposeMaterialTheme(content: @Composable() () -> Unit) {
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        colors = LightColor,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
     )
 }
